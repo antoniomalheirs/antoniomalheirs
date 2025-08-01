b18cb673538197cd8c48541e88f30cbe0e7ca6f3
Antônio Malheiros <79883711+antoniomalheirs@users.noreply.github.com>
2025-08-01 01:34:54 -0300

    Update update_repos.py

--- scripts/update_repos.py
+++ scripts/update_repos.py
@@ -23,10 +23,8 @@ Graduated in Computer Science from Faculdade Adamantinense Integrada - FAI, Tech
     <tr style="border: none;">
       <td style="border: none; vertical-align: top; padding-right: 20px;">
         <div style="text-align: center;">
-            <a href="https://www.instagram.com/malheirosan/" target="_blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram"/></a>
             <a href="https://www.linkedin.com/in/antonio-malheiros-68ba55226/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn"/></a>
             <a href="https://www.google.com/intl/pt-BR/gmail/about/" target="_blank"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail"/></a>
-            <br/><br/>
             <img src="https://komarev.com/ghpvc/?username=antoniomalheirs&abbreviated=true&style=for-the-badge&color=dc143f" alt="Profile Views"/>
         </div>
       </td>
