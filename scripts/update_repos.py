b967c13ad92a5777d3576e3e482caeb770e3f090
Antônio Malheiros <79883711+antoniomalheirs@users.noreply.github.com>
2026-07-14 13:55:24 -0300

    Add files via upload

--- scripts/update_repos.py
+++ scripts/update_repos.py
@@ -23,20 +23,21 @@ README_TEMPLATE = """<img width=100% src="https://capsule-render.vercel.app/api?
 | |              | | | |              | | | |              | | | |              | | | |              | | | |             | |
 | '--------------' | | '--------------' | | '--------------' | | '--------------' | | '--------------' | | '-------------' |
  '----------------'   '----------------'   '----------------'   '----------------'   '----------------'   '---------------'
-============================================================================================================================
-|                                                        About Me                                                          |
-============================================================================================================================
+==========================================================================================================================
+|                                                        About Me                                                        |
+==========================================================================================================================
 ```
-<div align="left">
-  <table border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse;">
+<div align="center">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse;">
     <tr style="border: none;">
-      <td height="55.00%" style="border: none; padding: 10px; text-align: center; display: flex; flex-direction: column; justify-content: center;">
+      <td width="60%" align="left" valign="middle" style="border: none; padding: 10px;">
         <ul>
           <li><b>Bacharel em Ciência da Computação</b>
             <ul>
               <li>Faculdade Adamantinense Integrada (FAI)</li>
             </ul>
           </li>
+          <br>
           <li><b>Técnico em Redes de Computadores e Infraestrutura</b>
             <ul>
               <li>Etec Prof. Eudécio Luiz Vicente - Centro Paula Souza</li>
@@ -44,11 +45,12 @@ README_TEMPLATE = """<img width=100% src="https://capsule-render.vercel.app/api?
           </li>
         </ul>
       </td>
-      <td height="45.00%" style="border: none; padding: 10px; text-align: center; display: flex; flex-direction: column; justify-content: center;">
-          <a href="https://www.instagram.com/malheirosan" target="_blank"><img align="center" src="https://github.com/Kourva/AwesomeBadges/blob/main/Badges/social/instagram.png" alt="Instagram" height="100" width="100" /></a>
-          <a href="https://discord.com/users/ID" target="_blank"><img align="center" src="https://github.com/Kourva/AwesomeBadges/blob/main/Badges/social/discord.png" alt="Discord" height="100" width="100" /></a>
-          <a href="https://t.me/unkandata" target="_blank"><img align="center" src="https://github.com/Kourva/AwesomeBadges/blob/main/Badges/social/telegram.png" alt="Telegram" height="100" width="100" /></a>
-          <a href="https://www.linkedin.com/in/SEU-USUARIO-AQUI" target="_blank"><img align="center" src="https://github.com/Kourva/AwesomeBadges/blob/main/Badges/social/linkedin.png" alt="Linkedin" height="100" width="100" /></a>
+      <td width="40%" align="center" valign="middle" style="border: none; padding: 20px;">
+          <a href="https://www.instagram.com/malheirosan" target="_blank"><img align="center" src="https://raw.githubusercontent.com/Kourva/AwesomeBadges/main/Badges/social/instagram.png" alt="Instagram" width="80" style="margin: 5px;"/></a>
+          <a href="https://discord.com/users/ID" target="_blank"><img align="center" src="https://raw.githubusercontent.com/Kourva/AwesomeBadges/main/Badges/social/discord.png" alt="Discord" width="80" style="margin: 5px;"/></a>
+          <br>
+          <a href="https://t.me/unkandata" target="_blank"><img align="center" src="https://raw.githubusercontent.com/Kourva/AwesomeBadges/main/Badges/social/telegram.png" alt="Telegram" width="80" style="margin: 5px;"/></a>
+          <a href="https://www.linkedin.com/in/SEU-USUARIO-AQUI" target="_blank"><img align="center" src="https://raw.githubusercontent.com/Kourva/AwesomeBadges/main/Badges/social/linkedin.png" alt="Linkedin" width="80" style="margin: 5px;"/></a>
       </td> 
     </tr>
   </table>
@@ -58,85 +60,78 @@ README_TEMPLATE = """<img width=100% src="https://capsule-render.vercel.app/api?
 <!--END_SECTION:waka-->
 
 ```text
-   .----------------------------------------------------------------------------------------------------------------------.
-  | /   \                                                                                                           /   \ |
- | | .-. |                                                                                                         | .-. | |
- | | | | |                                 ♫    S K I L L S,  S T A T S  &  M U S I C    ⚙                         | || | |
- | | '-' |                                                                                                         | '-' | |
-  | \   /                                                                                                           \   / |
-   '----------------------------------------------------------------------------------------------------------------------'
+  .--------------------------------------------------------------------------------------------------------------------.
+ | /   \                                                                                                          /   \ |
+| | .-. |                                                                                                        | .-. | |
+| | | | |                            ♫    S K I L L S,  S T A T S  &  M U S I C    ⚙                             | | | | |
+| | '-' |                                                                                                        | '-' | |
+ | \   /                                                                                                          \   / |
+  '--------------------------------------------------------------------------------------------------------------------'
 ```
 <div align="center">
-  <table border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse;">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse;">
     <tr style="border: none;">
-      <td style="border: none; vertical-align: top; ">
-        <img src="https://github-stats-alpha.vercel.app/api?username=antoniomalheirs&cc=292C34&tc=CD6D73&ic=91B674&bc=292C34" card_width="400" alt="GitHub Stats_Profile">
-        <br> 
-        <img src="https://github-readme-streak-stats.herokuapp.com?user=antoniomalheirs&theme=onedark&hide_border=true" card_width="350" alt="GitHub Streak">
+      <td width="33%" align="center" valign="middle" style="border: none; padding: 10px;">
+        <img src="https://github-stats-alpha.vercel.app/api?username=antoniomalheirs&cc=292C34&tc=CD6D73&ic=91B674&bc=292C34" width="350" alt="GitHub Stats_Profile">
+        <br><br>
+        <img src="https://github-readme-streak-stats.herokuapp.com?user=antoniomalheirs&theme=onedark&hide_border=true" width="350" alt="GitHub Streak">
       </td>
-      <td style="border: none; vertical-align: top; ">
+      <td width="33%" align="center" valign="middle" style="border: none; padding: 10px;">
         <img src="https://spotify-recently-played-readme.vercel.app/api?user=zeca09&width=320&count=7" alt="Spotify Recently Played"/>
       </td> 
-      <td style="border: none; vertical-align: top; ">
-        <img src="https://github-readme-stats.vercel.app/api?username=antoniomalheirs&show_icons=true&theme=onedark&hide_border=true&hide_title=false&include_all_commits=true" card_width="400" alt="GitHub Stats_Profile_Two">
-        <br>
-        <img src="https://streak-stats.demolab.com/?user=antoniomalheirs&theme=onedark&hide_border=true&mode=weekly" card_width="350" alt="GitHub Streak_Week" />
+      <td width="33%" align="center" valign="middle" style="border: none; padding: 10px;">
+        <img src="https://github-readme-stats-eight-theta.vercel.app/api?username=antoniomalheirs&show_icons=true&theme=onedark&hide_border=true&hide_title=false" width="350" alt="GitHub Stats_Profile_Two">
+        <br><br>
+        <img src="https://streak-stats.demolab.com/?user=antoniomalheirs&theme=onedark&hide_border=true&mode=weekly" width="350" alt="GitHub Streak_Week" />
       </td>
     </tr>
   </table>
 </div>
 
 ```text
-o--------------------------------------------------------------------------------------------------------------------------o
-|  [+]                                                                                                                [+]  |
-|  [+]                                        T O O L I N G   &   A B I L I T I E S                                   [+]  |
-|  [+]                                                                                                                [+]  |
-o--------------------------------------------------------------------------------------------------------------------------o
+o------------------------------------------------------------------------------------------------------------------------o
+|  [+]                                                                                                              [+]  |
+|  [+]                                   T O O L I N G   &   A B I L I T I E S                                      [+]  |
+|  [+]                                                                                                              [+]  |
+o------------------------------------------------------------------------------------------------------------------------o
 ```
 <div align="center">
-  <table border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse; height: 100%;">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" style="border: none; border-collapse: collapse;">
     <tr style="border: none;">
-      <td width="21.33%" style="border: none; text-align: center; display: flex; flex-direction: column; justify-content: center;">
-        <div align="center">
-          <h4>Programming Languages</h4>
-          <img src="https://skillicons.dev/icons?i=java,js,c,cpp,cs,py,bash&perline=4" alt="Programming Languages"/>
-        </div>
-        <div align="center" style="margin-top: 20px;">
-          <h4>Web & Backend Development</h4>
-          <img src="https://skillicons.dev/icons?i=nodejs,npm,express,tailwind,mongodb,mysql,firebase,sqlite&perline=4" alt="Web & Backend Development"/>
-        </div>
+      <td width="30%" align="center" valign="middle" style="border: none; padding: 15px;">
+        <h4>Programming Languages</h4>
+        <img src="https://skillicons.dev/icons?i=java,js,c,cpp,cs,py,bash&perline=4" alt="Programming Languages"/>
+        <br><br>
+        <h4>Web & Backend Development</h4>
+        <img src="https://skillicons.dev/icons?i=nodejs,npm,express,tailwind,mongodb,mysql,firebase,sqlite&perline=4" alt="Web & Backend Development"/>
       </td>
-      <td width="33.33%" style="border: none; text-align: center; display: flex; flex-direction: column; justify-content: center;">
-        <div align="center">
-          <img src="./public/top-languages.gif" width="420" height="230" alt="Dynamic Top Languages">
-          <img src="./public/stats.gif" width="380" height="230" alt="Dynamic Top Stats">
-        </div>
+      <td width="40%" align="center" valign="middle" style="border: none; padding: 15px;">
+        <img src="./public/top-languages.gif" width="420" height="230" alt="Dynamic Top Languages">
+        <br><br>
+        <img src="./public/stats.gif" width="380" height="230" alt="Dynamic Top Stats">
       </td>
-      <td width="21.33%" style="border: none; text-align: center; display: flex; flex-direction: column; justify-content: center;">
-        <div align="center">
-          <h4>Tools & Platforms</h4>
-          <img src="https://skillicons.dev/icons?i=git,github,githubactions,androidstudio,gcp,vscode,visualstudio,linux&perline=4" alt="Tools & Platforms"/>
-        </div>
-        <div align="center" style="margin-top: 20px;">
-          <h4>Other Skills</h4>
-          <img src="https://skillicons.dev/icons?i=discordjs,bots,unity,arduino,&perline=4" alt="Other Skills"/>
-        </div>
+      <td width="30%" align="center" valign="middle" style="border: none; padding: 15px;">
+        <h4>Tools & Platforms</h4>
+        <img src="https://skillicons.dev/icons?i=git,github,githubactions,androidstudio,gcp,vscode,visualstudio,linux&perline=4" alt="Tools & Platforms"/>
+        <br><br>
+        <h4>Other Skills</h4>
+        <img src="https://skillicons.dev/icons?i=discordjs,bots,unity,arduino,&perline=4" alt="Other Skills"/>
       </td>
     </tr>
   </table>
 </div>
 
 ```text
-<//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////>
-|                                                                                                                          |
-|                                           K N O W L E D G E   O F   L A N G U A G E S                                    |
-|                                                                                                                          |
-<//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////>
+<////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////>
+|                                                                                                                        |
+|                                      K N O W L E D G E   O F   L A N G U A G E S                                       |
+|                                                                                                                        |
+<////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////>
 ```
 <div align="center">
     <img src="./profile-3d-contrib/profile-night-rainbow.svg" alt="3D Contribution Graph"/>
 </div>
-
+<br>
 <p align="center">
   <picture>
     <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/antoniomalheirs/antoniomalheirs/output/github-contribution-grid-snake-dark.svg">
@@ -146,16 +141,17 @@ o-------------------------------------------------------------------------------
 </p>
 
 ```text
-   ______________________________________________________________________________________________________________________
- /     _______________________________________________________________________________________________________________    \\ 
-|   //                                                                                                                |    |
-|  |                                S O M E   I N T E R E S T I N G   R E P O S I T O R I E S                         |    |
-|  |_________________________________________________________________________________________________________________/     |
- \\________________________________________________________________________________________________________________________/
+   ____________________________________________________________________________________________________________________
+ /    ______________________________________________________________________________________________________________   \\ 
+|  //                                                                                                               |   |
+| |                             S O M E   I N T E R E S T I N G   R E P O S I T O R I E S                           |   |
+| |_________________________________________________________________________________________________________________/   |
+ \\______________________________________________________________________________________________________________________/
 ```
 
 {repo_section}
 
+<br>
 <img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=91B674&height=120&section=footer"/>
 """
 
@@ -179,9 +175,9 @@ def get_repos(username):
 def generate_repo_markdown(repo):
     """Gera a linha de markdown para um card de repositório."""
     repo_name = repo['name']
-    url = f"https://github-readme-stats.vercel.app/api/pin/?username={GITHUB_USERNAME}&repo={repo_name}&{CARD_PARAMS}"
+    url = f"https://github-readme-stats-eight-theta.vercel.app/api/pin/?username={GITHUB_USERNAME}&repo={repo_name}&{CARD_PARAMS}"
     link = f"https://github.com/{GITHUB_USERNAME}/{repo_name}"
-    return f'<a href="{link}"><img src="{url}" alt="{repo_name}" width="400"/></a>'
+    return f'<a href="{link}"><img src="{url}" alt="{repo_name}" width="400" style="margin: 5px;"/></a>'
 
 # Bloco de execução principal do script
 if __name__ == "__main__":
@@ -200,12 +196,12 @@ if __name__ == "__main__":
 
                 if i + 1 < len(random_repos):
                     repo2 = random_repos[i+1]
-                    line += "\n" + generate_repo_markdown(repo2)
+                    line += "\n  " + generate_repo_markdown(repo2)
 
                 markdown_lines.append(line)
 
-            repo_cards_markdown = "\n<br><br>\n".join(markdown_lines)
-            repo_cards_markdown = f'<div align="center">\n{repo_cards_markdown}\n</div>'
+            repo_cards_markdown = "\n  <br/>\n  ".join(markdown_lines)
+            repo_cards_markdown = f'<div align="center">\n  {repo_cards_markdown}\n</div>'
 
     # Preenche o template com a seção de repositórios gerada
     final_readme = README_TEMPLATE.format(repo_section=repo_cards_markdown)
@@ -215,25 +211,3 @@ if __name__ == "__main__":
         f.write(final_readme)
 
     print("README.md reescrito com sucesso a partir do template final!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
